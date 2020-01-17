735afe178e267692271ac673e1233a91ef23788b
Dean Lovett <dean.lovett@gmail.com>
2020-01-17 15:10:26 -0800

    Fixes WYSIWYG styling, connects to DB, pushes HTML to DB
    * Added a working wysiwyg, writing to db
    
    * added code to convert editor contents to html
    
    Co-authored-by: Roy "sdcs" Hobbs <51515566+royhobbs9@users.noreply.github.com>

--- client/js/index.js
+++ client/js/index.js
@@ -2,11 +2,12 @@
 import React, { useState, useEffect } from "react"
 import ReactDom from "react-dom"
 import axios from "axios"
-import { Editor, EditorState } from 'draft-js';
 import '../css/App.css'
 
-//** COMPONENTS **//
-// import WYSIWYG from './Components/WYSIWYG.js'
+import { Editor } from 'react-draft-wysiwyg';
+import { EditorState, convertToRaw} from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 export default function Index() {
 
@@ -15,25 +16,16 @@ export default function Index() {
     const [imagePath, setImagePath] = useState("")
     const [editorState, setEditorState] = useState(EditorState.createEmpty())
 
-    const editor = React.useRef(null);
-
-    function focusEditor() {
-        editor.current.focus();
-    }
-
-    React.useEffect(() => {
-        focusEditor()
-    }, []);
-
     let valueHandler = () => {
         axios.post("http://localhost:5000/api/blogs", {
             title: title,
             date: Date.now(),
             author: author,
             imagePath: imagePath,
-            content: editorState
+            content: JSON.stringify(editorState)
         })
-            .then((response) => console.log(response))
+            .then((response) => console.log(`Response`,response)); // `\n\neditorstate.getCurrentContent ${JSON.stringify(convertToRaw(editorState.getCurrentContent()))}`))
+            // console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())));
     }
 
     const divStyle = {
@@ -49,18 +41,21 @@ export default function Index() {
             <input id="author" onChange={e => setAuthor(e.target.value)}></input>
             <label htmlFor="imagePath">Image Path</label>
             <input id="imagePath" onChange={e => setImagePath(e.target.value)}></input>
-            <div onClick={focusEditor}>
-                <Editor
-                    ref={editor}
-                    editorState={editorState}
-                    onChange={editorState  => setEditorState(editorState)}
-                />
+            <div>
+            <Editor
+                editorState={editorState}
+                toolbarClassName="toolbarClassName"
+                wrapperClassName="wrapperClassName"
+                editorClassName="editorClassName"
+                onEditorStateChange={setEditorState}
+            />
             </div>
             <button onClick={valueHandler}>Save</button>
+            <> 
+            {draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+            </>
         </div>
-  
     )
-
 }
 
 ReactDom.render(<Index />, document.getElementById("root"))
